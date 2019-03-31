b752be46cba9050703f6f879835da9983223eb7b
SHASHANK BHAT <shashankbhat1800@gmail.com>
2019-03-31 17:42:05 +0530

    Version 1.2

--- app/src/main/java/com/rnsit/utopia/Contact.java
+++ app/src/main/java/com/rnsit/utopia/Contact.java
@@ -36,7 +36,7 @@ public class Contact extends AppCompatActivity{
         mCoordinatorAdapter = new CoordinatorAdapter(context, mCoordinatorObject);
         rec_coordinators.setAdapter(mCoordinatorAdapter);
         mCoordinatorObject.add(new CoordinatorObject("Cultural",R.drawable.cultboy,"Rahul Shetkar","8880877635",R.drawable.cultgirl,"Apurva Choudhary","8971824830"));
-        mCoordinatorObject.add(new CoordinatorObject("Fun",R.drawable.funboy,"Sharan Chhibber","7044142526",R.drawable.fungirl,"Thejaswini","7259158445"));
+        mCoordinatorObject.add(new CoordinatorObject("Fun",R.drawable.funboy,"Sharan Chhibber","7044142526",R.drawable.fungirl,"Thejaswini","7676086479"));
         mCoordinatorObject.add(new CoordinatorObject("Literature",R.drawable.litboy,"Piyush Rana","9986524394",R.drawable.litgirl,"Meghana V Patil","9900207355"));
         mCoordinatorObject.add(new CoordinatorObject("Sports",R.drawable.sportsboy,"Eekshith","9490453239",R.drawable.sportsgirl,"Niveditha","9206609997"));
         mCoordinatorObject.add(new CoordinatorObject("Technical",R.drawable.techboy,"Varun Harish","9986780267",R.drawable.techgirl,"Sheetal Vernekar","9632660327"));
